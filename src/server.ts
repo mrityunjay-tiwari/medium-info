ae4d0332eb812d13d75c6c3e18753ce198b6e334
mrityunjay_ <mrityunjaytiwari1873@gmail.com>
2025-11-09 10:29:07 +0530

    adding_api

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import express from "express";
+import cors from "cors";
+import { getArticleInfo, getAuthorAvatar } from "medium-info-api";
+
+const app = express();
+app.use(cors());
+
+app.get("/medium", async (req, res) => {
+  const url = req.query.url as string;
+
+  if (!url) {
+    return res.status(400).json({ error: "Missing url parameter" });
+  }
+
+  try {
+    const result = await getArticleInfo(url);
+    console.log("result , ", result);
+    return res.json({ success: true, data: result, message: "For authorAvatar, visit : https://localhost:3000/medium/avatar?url=<medium_article_url>" });
+  } catch (err) {
+    return res.status(500).json({ success: false, message: "Failed to fetch article", error: err });
+  }
+});
+
+app.get("/medium/avatar", async (req, res) => {
+  const url = req.query.url as string;
+
+  if (!url) {
+    return res.status(400).json({ error: "Missing url parameter" });
+  }
+
+  try {
+    const result = await getAuthorAvatar(url);
+    console.log("result , ", result);
+    return res.json({ success: true, data: result, message: "For all other informations, visit : https://localhost:3000/medium?url=<medium_article_url>" });
+  } catch (err) {
+    return res.status(500).json({ success: false, message: "Failed to fetch author avatar", error: err });
+  }
+});
+
+export default app
